ad9375b680ab92ed93d06dedf59dec74bf333729
Kim Dang & Jesse James <student@epicodus.com>
2015-04-28 09:55:47 -0700

    Add content to webpage erb files.

--- spec/to_do_integration_spec.rb
+++ spec/to_do_integration_spec.rb
@@ -6,6 +6,8 @@ set(:show_exceptions, false)
   describe('To_Do user path', {:type => :feature}) do
     it('verifies the user path from the form to results page') do
       visit('/')
-      expect(page).to have_content('To Do List')
+      fill_in('description', :with => 'clean car')
+      click_button('Add Task')
+      expect(page).to have_content('The task has been successfully submitted! Back')
     end
   end
